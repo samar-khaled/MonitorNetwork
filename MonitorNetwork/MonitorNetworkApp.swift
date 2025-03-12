5ca8abe568f29fd615177556e7da1c56ad74cb37
Samar Khaled <samar.khaled@gmail.com>
2025-03-12 13:11:30 +0200

    Initial Commit

---
+++ MonitorNetwork/MonitorNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorNetworkApp.swift
+//  MonitorNetwork
+//
+//  Created by Samar Khaled on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MonitorNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
